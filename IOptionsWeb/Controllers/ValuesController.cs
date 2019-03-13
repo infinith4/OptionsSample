079f2c002a000aeb0c203371ba45e860c34975c2
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2019-03-13 22:48:18 +0900

    get options

---
+++ IOptionsWeb/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IOptionsWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace IOptionsWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly MyOptions _options;
+        private readonly MyOptionsWithDelegateConfig _optionsWithDelegateConfig;
+        private readonly MyOptions _snapshotOptions;
+        private readonly MyOptions _named_options_1;
+        private readonly MyOptions _named_options_2;
+
+        public ValuesController(
+            IOptionsMonitor<MyOptions> optionsAccessor,
+            IOptionsMonitor<MyOptionsWithDelegateConfig> optionsAccessorWithDelegateConfig,
+            IOptionsSnapshot<MyOptions> snapshotOptionsAccessor,
+            IOptionsSnapshot<MyOptions> namedOptionsAccessor)
+        {
+            _options = optionsAccessor.CurrentValue;
+            _optionsWithDelegateConfig = optionsAccessorWithDelegateConfig.CurrentValue;
+            _snapshotOptions = snapshotOptionsAccessor.Value;
+            _named_options_1 = namedOptionsAccessor.Get("named_options_1");
+            _named_options_2 = namedOptionsAccessor.Get("named_options_2");
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IOptionsWeb/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IOptionsWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the service required for using options.
+            services.AddOptions();
+
+            #region snippet_Example1
+            // Example #1: General configuration
+            // Register the Configuration instance which MyOptions binds against.
+            services.Configure<Models.MyOptions>(Configuration);
+            #endregion
+
+            #region snippet_Example2
+            // Example #2: Options bound and configured by a delegate
+            services.Configure<Models.MyOptionsWithDelegateConfig>(myOptions =>
+            {
+                myOptions.Option1 = "value1_configured_by_delegate";
+                myOptions.Option2 = 500;
+            });
+            #endregion
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
